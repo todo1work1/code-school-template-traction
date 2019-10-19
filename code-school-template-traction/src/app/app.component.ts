6dd7d362531e17839d3ca4e67cf60924a195820f
todo1work1 <todo1work1@gmail.com>
2019-10-19 11:18:33 +0530

    completed section template traction

--- code-school-template-traction/src/app/app.component.ts
+++ code-school-template-traction/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'code-school-template-traction';
+  title = 'Ultra Racing';
+  cars=[
+    {
+      "id": 1,
+      "name": "Super Tires",
+      "description": "These tires are the very best",
+      "inStock": 5,
+      "price": 4.99
+      },
+      {
+        "id": 2,
+        "name": "Reinforced Shocks",
+        "description": "Shocks made from kryptonite",
+        "inStock": 4,
+        "price": 9.99
+      },
+      {
+        "id": 3,
+        "name": "Padded Seats",
+        "description": "Super soft seats for a smooth ride",
+        "inStock": 0,
+        "price": 24.99
+      }
+  ];
+
+  calcTotalParts() {
+    let totalParts =0;
+    this.cars.forEach(car => {
+      totalParts+=car.inStock;
+    });
+    return totalParts;
+  }
+
+  calcTotalPrice() {
+    return this.cars.reduce((prev,cur) => prev +  cur.price, 0);
+  }
 }
